252cf4c279b5b58778228b5c8b133ee8268fb815
ourbest <chenyonghui@gmail.com>
2018-07-21 20:19:36 +0800

    wrong port

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ def create_app():
     # 这个工厂方法可以从你的原有的 `__init__.py` 或者其它地方引入。
     app = serv.app
     try:
-        cutt_sd.http_register('/spi/url', 8388)
+        cutt_sd.http_register('/spi/url', 8838)
     except:
         app.logger.warning("Error register service", exc_info=1)
     return app
