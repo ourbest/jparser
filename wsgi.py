c7f7954c1c4038818b2439d336bd906455aa708c
ourbest <chenyonghui@gmail.com>
2018-07-21 17:32:10 +0800

    register /spi/url/article

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ def create_app():
     # 这个工厂方法可以从你的原有的 `__init__.py` 或者其它地方引入。
     app = serv.app
     try:
-        cutt_sd.http_register('/url/article', 8388)
+        cutt_sd.http_register('/spi/url/article', 8388)
     except:
         app.logger.warning("Error register service", exc_info=1)
     return app
